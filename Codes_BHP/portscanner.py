c45597ef372cac939ca09063e01d7ee4e6705534
Spike <72819608+SpikeLibertarian@users.noreply.github.com>
2023-02-12 17:48:36 -0300

    portscanner.py
    This basic script is designed to scan connection ports and tell you whether they are open or closed.

---
+++ Codes_BHP/portscanner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import socket;
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+
+host = (input("Insert the host:")); 
+port = int(input("Insert the port:"));
+
+def portscanner(port):
+  if sock.connect_ex((host,port)):
+    print("Port %d is closed" % (port) );
+  else:
+      print("Port %d is opened" % (port));
+portscanner(port);
